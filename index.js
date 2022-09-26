6b17d64c63694de893398a70852563e1c2251c74
Staci Rosales <stacileep2@gmail.com>
2022-09-26 15:16:05 -0500

    commit tree 71747b9a0c4e0fef2e4d9bb7d62dc3a975db183b

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(y) {
+    if (y > 42) {
+        return y - 42;
+    } else if (y < 42) {
+        return 42 - y; 
+    }
+}
+function distanceFromHqInFeet(y) {
+    return distanceFromHqInBlocks(y) * 264;
+}
+function distanceTravelledInFeet(start, end) {
+    if (start > end) {
+        return (start - end) * 264;
+    } else if (start < end) {
+        return (end - start) * 264;
+    }
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination); 
+    if (distance < 400) {
+        return 0 ;
+    } else if (400 < distance && distance < 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+}
+    
